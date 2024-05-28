2e069c2d3e9e76c9a0590b0eefc827b21b615da6
JRCoscolo <coscolinguada@gmail.com>
2024-05-28 18:49:20 +0200

    Ejercicio 2

--- ejercicio.js
+++ ejercicio.js
@@ -1,2 +1,8 @@
-let colors = ["rojo", "verde", "azul", "amarillo"];
-console.log(colors[2]);
+//Ejercicio 1.
+//- Crea un array llamado `colors` con los colores "rojo", "verde", "azul", "amarillo".
+//- Imprime el tercer color del array.
+let colors = ["rojo", "verde", "azul", "amarillo"]
+console.log(colors[2])
+//Ejercicio 2.
+//- Imprime el último elemento del array `colors` sin usar su índice explícitamente.
+console.log(colors[colors.length - 1])
