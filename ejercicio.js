0bd7682ac0f730316cb13e6f1d7a05071c20ca2b
JRCoscolo <coscolinguada@gmail.com>
2024-05-30 23:42:33 +0200

    Ejercicio 12

--- ejercicio.js
+++ ejercicio.js
@@ -1,8 +1,11 @@
-//Ejercicio 11
-//- Crear y Acceder a Arrays
-//Crea un array llamado `fruits` que contenga las frutas "manzana", "banana" y "naranja"
-//Imprime el primer y el último elemento del array.
+//Ejercicio 12
+//- Modificar Arrays
+//Añade "uva" al final del array `fruits`
+//Cambia el segundo elemento del array a "kiwi"
+//Elimina el primer elemento del array
 
 let fruits = ["manzana", "banana", "naranja"];
-console.log(fruits[0])
-console.log(fruits[2])
+fruits.push("uva") //push sirve para "empujar" un nuevo elemento al array, por defecto lo pondrá al final.
+fruits[1] = "kiwi" //el cambio del valor de array es muy obvio aqui
+fruits.shift() //la eliminación de un elemento de array, si no se pone nada, por defecto será el primero.
+console.log(fruits)
