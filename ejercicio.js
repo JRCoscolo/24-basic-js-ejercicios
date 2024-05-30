96d85094bbf27b9baf926e42ec20695085a88f5b
JRCoscolo <coscolinguada@gmail.com>
2024-05-30 23:37:04 +0200

    Ejercicio 10

--- ejercicio.js
+++ ejercicio.js
@@ -1,21 +1,24 @@
-//Ejercicio 8
-//- Transponer una Matriz
-//Escribe una función que reciba una matriz y devuelva su transpuesta (intercambiar filas por columnas)
-
+//Ejercicio 10
+//- Rotar una Matriz 90 Grados
+//Escribe una función que rote una matriz cuadrada 90 grados en el sentido de las agujas del reloj
 
 let colors = ["rojo", "verde", "azul", "amarillo"]
 let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-function transponer(matrix) {
-    let transpuesta = [];
-    for (let i = 0; i < matrix[0].length; i++) {
-        transpuesta[i] = [];
-        for (let j = 0; j < matrix.length; j++) {
-            transpuesta[i][j] = matrix[j][i];
+function rotarmatrix(matrix) {
+    let n = matrix.length;
+    let rotado = []; //Dejamos hueco para que la posición nos la vaya marcando el bucle
+    for (let i = 0; i < n; i++) {
+        rotado[i] = []; //Dejamos hueco para que la posición nos la vaya marcando el bucle
+        for (let j = 0; j < n; j++) {
+            rotado[i][j] = matrix[n - j - 1][i];
         }
     }
-    return transpuesta;
+    return rotado;
 }
 
-let transposedmatrix = transponer(matrix)
-console.log(transposedmatrix)
+let matrixrotada = rotarmatrix(matrix)
+console.log(matrixrotada)
+
+// Queda anotado que la función "i" determina la cacheta del array, y 
+//la "j" la posición del elemento de dicho array.
