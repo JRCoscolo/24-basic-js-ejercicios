40082b5ded6be7c7a64a2c5d998f2090670a9902
JRCoscolo <coscolinguada@gmail.com>
2024-05-30 23:07:25 +0200

    Ejercicio 4

--- ejercicio.js
+++ ejercicio.js
@@ -1,13 +1,8 @@
-//Ejercicio 3
-//- Crea una matriz `matrix` que represente la siguiente tabla:
+//Ejercicio 4
+//- Cambia los elementos de una Matriz.
+//Cambia el elemento de la fila 1 y segunda columna a 20
 
-//``` javascript
-//1 2 3
-//4 5 6
-//7 8 9
-//```
 
-//- Imprime el número que se encuentra en la segunda fila y tercera columna.
 let matrix = [[1,2,3], [4,5,6], [7,8,9]]
-        
-console.log(matrix[1][2]);
+ matrix[0][1] = 20       
+console.log(matrix)
