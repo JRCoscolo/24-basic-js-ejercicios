5dc7c0971cd8b88cdf4dcd85d81f9711e1c52040
JRCoscolo <coscolinguada@gmail.com>
2024-05-30 23:22:43 +0200

    Ejercicio 7

--- ejercicio.js
+++ ejercicio.js
@@ -1,12 +1,18 @@
-//Ejercicio 6
-//- Recorrer un Arreglo.
-//Usa un bucle `for` anidado para imprimir todos los elementos de la matriz `matrix`
+//Ejercicio 7
+//- Sumar Todos los Elementos de una Matriz.
+//Escribe una función que reciba una matriz y devuelva la suma de todos sus elementos
 
 
 let colors = ["rojo", "verde", "azul", "amarillo"]
 let matrix = [[1,2,3], [4,5,6], [7,8,9]]
-for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-        console.log(matrix[i][j]);
+function sumMatrix(matrix) {
+    let sum = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            sum += matrix[i][j];
+        }
     }
+    return sum
 }
+
+console.log(sumMatrix(matrix));
