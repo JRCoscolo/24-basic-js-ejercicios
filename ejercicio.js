c0a9e6964a8ea05b51beb83ae6a83759e52b147b
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-03 08:47:10 +0200

    Ejercicio 14

--- ejercicio.js
+++ ejercicio.js
@@ -1,11 +1,8 @@
-//Ejercicio 13
+//Ejercicio 14
 //- Uso de Métodos Básicos
-//Crea un array llamado `numbers` con los números del 1 al 5
-//Usa el método `map` para crear un nuevo array que contenga el cuadrado de cada número
-//Usa el método `filter` para crear un nuevo array que contenga solo los números mayores que 2
+//Usa un bucle `for` para imprimir cada elemento del array `fruits` en mayúsculas
 
-let numbers = [1, 2, 3, 4, 5]
-let squares = numbers.map((num) => num * num) //Aquí metemos el método "map"
-let greaterThanTwo = numbers.filter((num) => num > 2) //Y aquí discriminamos los números mayores que 2
-console.log(squares)
-console.log(greaterThanTwo)
+let fruits = ["manzana", "banana", "naranja"]
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i].toUpperCase());
+  }
